ca968b06e3d53c854905aa10b65ad7a8b31b2c96
KulturistDeveloper <Peter6819762@gmail.com>
2022-03-29 16:35:35 +0300

    TuduList_v2

--- src/TodoListTemplate.js
+++ src/TodoListTemplate.js
@@ -3,7 +3,7 @@ import "./TodoListTemplate.css";
 const TodoListTemplate = ({ form, children, palette }) => {
   return (
     <main className="todo-list-template">
-      <div className="title">Добро пожаловать в TuduList</div>
+      <div className="title">Добро пожаловать в TodoList</div>
       <div className="form-wrapper">{form}</div>
       <div className="todos-wrapper">{children}</div>
       <div className="palette">{palette}</div>
